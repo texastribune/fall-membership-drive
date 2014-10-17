1a1c347d8c2f60529bf23f901c6f44860675d399
Kathryn Beaty <kabeaty@gmail.com>
2014-10-17 13:31:46 -0500

    Add in resize wrapper and jquery to resize so nav always visible; to get ready for this

--- source/js/main.js
+++ source/js/main.js
@@ -1,89 +1,94 @@
 $(document).ready(function() {
-        // Wrap videos and apply CSS for fluid width
-        $('.video').fitVids();
 
-        // For Texas Weekly counter
-        $('.counter').counterUp({
-          delay: 50,
-          time: 3000
-        });
+  // to use to resize landing so nav always visible on load
+  // uncomment after top image updated, but adding now to be ready
+  // $('.resize-wrapper').css('height', ($(window).innerHeight() - 50).toString());
+  
+  // Wrap videos and apply CSS for fluid width
+  $('.video').fitVids();
 
-        // For animation to anchor links
-        $('a[href*=#]:not([href=#])').click(function() {
-          if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-            if (target.length) {
-              $('html,body').animate({
-                scrollTop: target.offset().top
-              }, 1500);
-              return false;
-            }
-          }
-        });
+  // For Texas Weekly counter
+  $('.counter').counterUp({
+    delay: 50,
+    time: 3000
+  });
 
-        // Initialize the mobile slicknav menu
-        $('#menu').slicknav();
-      });
+  // For animation to anchor links
+  $('a[href*=#]:not([href=#])').click(function() {
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        $('html,body').animate({
+          scrollTop: target.offset().top
+        }, 1500);
+        return false;
+      }
+    }
+});
 
-      // For arrow transition between sections
-      $('.section-arrow').waypoint(function() {
-        $(this).addClass('bg-size-appear');
-      }, {offset: '30%'});
+  // Initialize the mobile slicknav menu
+  $('#menu').slicknav();
+});
 
-      // animates in 'Trib' and 'Talk'
-      $('.animate-from-left').waypoint(function() {
-        $(this).addClass('side-animate');
-      }, {offset: "50%"});
+// For arrow transition between sections
+$('.section-arrow').waypoint(function() {
+  $(this).addClass('bg-size-appear');
+}, {offset: '30%'});
 
-      $('.animate-from-left').waypoint(function() {
-        $(this).addClass('appear');
-      }, {offset: "30%"});
+// animates in 'Trib' and 'Talk'
+$('.animate-from-left').waypoint(function() {
+  $(this).addClass('side-animate');
+}, {offset: "50%"});
 
-      $('.animate-from-right').waypoint(function() {
-        $(this).addClass('side-animate');
-      }, {offset: "50%"});
+$('.animate-from-left').waypoint(function() {
+  $(this).addClass('appear');
+}, {offset: "30%"});
 
-      $('.animate-from-right').waypoint(function() {
-        $(this).addClass('appear');
-      }, {offset: "30%"});
+$('.animate-from-right').waypoint(function() {
+  $(this).addClass('side-animate');
+}, {offset: "50%"});
 
-      // Using waypoints to change active nav class
-      $('#story').waypoint(function() {
-        $('.header li').removeClass('active');
-        $('.header-link[href=#story]').closest('li').addClass('active');
-      }, {offset: 20});
+$('.animate-from-right').waypoint(function() {
+  $(this).addClass('appear');
+}, {offset: "30%"});
 
-      $('#innovation').waypoint(function() {
-        $('.header li').removeClass('active');
-        $('.header-link[href=#innovation]').closest('li').addClass('active');
-      }, {offset: 20});
+// Using waypoints to change active nav class
+$('#story').waypoint(function() {
+  $('.header li').removeClass('active');
+  $('.header-link[href=#story]').closest('li').addClass('active');
+}, {offset: 20});
 
-      $('#accountability').waypoint(function() {
-        $('.header li').removeClass('active');
-        $('.header-link[href=#accountability]').closest('li').addClass('active');
-      }, {offset: 20});
+$('#innovation').waypoint(function() {
+  $('.header li').removeClass('active');
+  $('.header-link[href=#innovation]').closest('li').addClass('active');
+}, {offset: 20});
 
-      $('#transparency').waypoint(function() {
-        $('.header li').removeClass('active');
-        $('.header-link[href=#transparency]').closest('li').addClass('active');
-      }, {offset: 20});
+$('#accountability').waypoint(function() {
+  $('.header li').removeClass('active');
+  $('.header-link[href=#accountability]').closest('li').addClass('active');
+}, {offset: 20});
 
-      $('#engagement').waypoint(function() {
-        $('.header li').removeClass('active');
-        $('.header-link[href=#engagement]').closest('li').addClass('active');
-      }, {offset: 20});
+$('#transparency').waypoint(function() {
+  $('.header li').removeClass('active');
+  $('.header-link[href=#transparency]').closest('li').addClass('active');
+}, {offset: 20});
 
-      $('#donate').waypoint(function() {
-        $('.header li').removeClass('active');
-        $('.header-link[href=#donate]').closest('li').addClass('active');
-      }, {offset: 20});
+$('#engagement').waypoint(function() {
+  $('.header li').removeClass('active');
+  $('.header-link[href=#engagement]').closest('li').addClass('active');
+}, {offset: 20});
 
-      $('#donate').waypoint(function() {
-        $(this).addClass('yellow-bg');
-      }, {offset: "30%"});
+$('#donate').waypoint(function() {
+  $('.header li').removeClass('active');
+  $('.header-link[href=#donate]').closest('li').addClass('active');
+}, {offset: 20});
 
-      // For sticky header
-      $('#unsticky-nav').waypoint(function() {
-        $('#sticky-nav').toggleClass('nav-hide');
-      });
+$('#donate').waypoint(function() {
+  $(this).addClass('yellow-bg');
+}, {offset: "30%"});
+
+// For sticky header
+$('#unsticky-nav').waypoint(function() {
+  $('#sticky-nav').toggleClass('nav-hide');
+});
